f0ef21be46799e931b451724686360f0ed5485b2
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2012-04-29 00:08:35 +0200

    changed from testLibs to testHelpers

--- buster-integration.js
+++ buster-integration.js
@@ -3,7 +3,7 @@ var config = module.exports;
 config['Integration tests'] = {
     env:'browser',
     libs:[ 'client_dependencies/**/jquery.js'],
-    testLibs :[ 'test/integration/test-helper.js'],
+    testHelpers :[ 'test/integration/test-helper.js'],
     tests:[ 'test/integration/**/*-test.js' ],
     resources: [{ path: "app", backend: "http://localhost:3000/" }]
 };
